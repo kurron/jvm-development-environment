85212e2f841dd4b6e59bd56e3208d82f38b73959
Ron Kurr <kurr@kurron.org>
2014-11-08 18:12:06 -0500

    Got JD 8 working

---
+++ ansible/scripts/download-jdk-8.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DONEFILE=/var/ansible-jdk-8
+
+# make sure we are idempotent
+if [ -f "${DONEFILE}" ]; then
+    exit 0
+fi
+
+wget --quiet --output-document=/tmp/jdk-8.tar.gz --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-linux-x64.tar.gz 
+
+# signal a successful provision
+touch ${DONEFILE}
