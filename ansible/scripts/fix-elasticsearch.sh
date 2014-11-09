1eb4755ee1f3325252d4f566f043d4a2bfd251f8
Ron Kurr <kurr@kurron.org>
2014-11-08 23:25:07 -0500

    Store downloaded file in /root

--- ansible/scripts/fix-elasticsearch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo Configuring cluster
-/bin/sed --in-place --expression 's/#cluster.name: elasticsearch/cluster.name: logstash/g' /etc/elasticsearch/elasticsearch.yml 
-
-echo Configuring node
-/bin/sed --in-place --expression 's/#node.name: "Franz Kafka"/node.name: "logstash"/g' /etc/elasticsearch/elasticsearch.yml 
