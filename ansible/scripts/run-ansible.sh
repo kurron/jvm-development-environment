5c8ba886d609766eef0ba729d512f2a37c0df126
Ron Kurr <kurr@kurron.org>
2015-03-26 19:56:30 -0400

    Continued to improve headless boxes

--- ansible/scripts/run-ansible.sh
+++ ansible/scripts/run-ansible.sh
@@ -2,11 +2,6 @@
 
 # we have to do this in case we are running under Windows
 cp -r /vagrant/ansible /tmp
-chmod -x /tmp/ansible/inventory.ini
-
 
 mkdir -p /opt/git
-# we have to checkout the repo by hand just to deal with Windows permissions!
-#git clone https://github.com/kurron/ansible-pull.git /opt/ansible-pull
-
 ansible-pull --checkout master --directory /opt/ansible-pull --inventory-file=/opt/ansible-pull/inventory.ini  --module-name=git  --only-if-changed --url=https://github.com/kurron/ansible-pull.git --verbose playbook.yml
