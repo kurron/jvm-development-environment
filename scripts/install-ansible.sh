32bc7a82ce4aec4d02abd3e16ee3c76854b79944
Ron Kurr <kurr@kurron.org>
2015-09-28 01:28:05 +0000

    Use pip to install Ansible

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -7,14 +7,8 @@ if [ -f "${DONEFILE}" ]; then
     exit 0
 fi
 
-sudo apt-get -y update
-sudo apt-get install -y python-yaml python-jinja2 git
-sudo git clone http://github.com/ansible/ansible.git --recursive /usr/lib/ansible
-
-sudo sed  --in-place "s/PATH=/#PATH=/" /etc/environment
-sudo echo 'PATH="/usr/lib/ansible/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"' >> /etc/environment
-sudo echo 'ANSIBLE_LIBRARY=/usr/lib/ansible/library' >> /etc/environment
-sudo echo 'PYTHONPATH=/usr/lib/ansible/lib:$PYTHON_PATH' >> /etc/environment
+sudo easy_install pip
+sudo pip install ansible
 
 # signal a successful provision
 touch ${DONEFILE}
