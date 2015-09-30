74c800a3dd8f1e5f389f7103a23fe6a3af53274a
Ron Kurr <kurr@kurron.org>
2015-09-30 02:30:20 +0000

    Removed old Ansible files

--- ansible/scripts/run-local-playbook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INVENTORY=$1
-# we have to do this in case we are running under Windows
-cp -r /vagrant/ansible /tmp
-chmod -x /tmp/ansible/*.ini
-
-ansible-playbook --verbose --connection=local --limit=${HOSTNAME} --inventory-file=/tmp/ansible/$INVENTORY /tmp/ansible/custom-playbook.yml
