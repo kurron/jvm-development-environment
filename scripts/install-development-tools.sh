bad36f501ca127552e9983aae185a381270e5211
Ron Kurr <kurr@kurron.org>
2015-09-30 03:03:37 +0000

    Use local inventory file

--- scripts/install-development-tools.sh
+++ scripts/install-development-tools.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo ansible-pull --checkout master --directory /opt/ansible-pull-development --inventory-file=/opt/ansible-pull-development/inventory  --module-name=git  --url=https://github.com/kurron/ansible-pull-development.git --verbose playbook.yml
+sudo ansible-pull --checkout master --directory /opt/ansible-pull-development --inventory-file=/tmp/inventory  --module-name=git  --url=https://github.com/kurron/ansible-pull-development.git --verbose playbook.yml
--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo ansible-pull --checkout master --directory /opt/ansible-pull-docker --inventory-file=/opt/ansible-pull-docker/inventory  --module-name=git  --url=https://github.com/kurron/ansible-pull-docker.git --verbose playbook.yml
+sudo ansible-pull --checkout master --directory /opt/ansible-pull-docker --inventory-file=/tmp/inventory  --module-name=git  --url=https://github.com/kurron/ansible-pull-docker.git --verbose playbook.yml
