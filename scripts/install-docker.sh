39c34dbc1a971dc2e4fe2df5547007f89d3ca830
Ron Kurr <kurr@kurron.org>
2016-04-22 17:35:28 -0400

    Switched back to the Trusty way of doing things

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo ansible-pull --checkout master --directory /opt/ansible-pull-docker --inventory-file=/tmp/inventory  --module-name=git  --url=https://github.com/kurron/ansible-pull-docker.git --verbose playbook.yml
+sudo ansible-pull --checkout trusty-x64-final --directory /opt/ansible-pull-docker --inventory-file=/tmp/inventory  --module-name=git  --url=https://github.com/kurron/ansible-pull-docker.git --verbose playbook.yml
