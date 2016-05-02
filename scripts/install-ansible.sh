5d7096d42723dbf6bd85f9dd9682cfc1db76240c
Ron Kurr <kurr@kurron.org>
2016-05-02 16:48:20 -0400

    Put in the Carl Fix just to be safe

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -7,6 +7,9 @@ if [ -f "${DONEFILE}" ]; then
     exit 0
 fi
 
+# I put this in because Carl's install got wedged once because it couldn't acquire the repository lock
+sudo apt-get update
+
 sudo apt-get install -y libssl-dev libffi-dev
 sudo easy_install pip
 sudo pip install ansible
