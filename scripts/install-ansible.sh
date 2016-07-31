9cff01773c099a7ecb2ae864a2ad4567bafafe7f
Ron Kurr <kurr@kurron.org>
2016-07-31 12:24:45 -0400

    Use a new technique to install pip

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -10,9 +10,14 @@ fi
 # I put this in because Carl's install got wedged once because it couldn't acquire the repository lock
 sudo apt-get update
 
-sudo apt-get install -y libssl-dev libffi-dev
-sudo easy_install pip
-sudo pip install ansible
+#sudo apt-get install -y libssl-dev libffi-dev
+#sudo easy_install pip
+
+# supposedly, this is the newer way to install pip
+sudo apt-get install -y python-pip python-dev build-essential libssl-dev libffi-dev
+sudo pip install --upgrade pip
+sudo pip install --upgrade ansible
+#sudo pip install --upgrade boto
 
 # signal a successful provision
 touch ${DONEFILE}
