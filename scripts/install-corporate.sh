2001bf0776df0d2681308780b6172b12df90b71d
Ron Kurr <kurr@kurron.org>
2016-11-04 21:54:40 -0400

    Specify the user

--- scripts/install-corporate.sh
+++ scripts/install-corporate.sh
@@ -9,5 +9,13 @@ fi
 # the GitHub location of the custom Ansible plays to run eg, https://github.com/kurron/ansible-pull-transparent.git 
 PROJECT=$1
 
-ansible-pull --checkout master --directory /opt/ansible-pull-corporate --inventory-file=/tmp/inventory --module-name=git --url=https://github.com/${PROJECT} --verbose playbook.yml
+ansible-pull --checkout master \
+             --directory /opt/ansible-pull-corporate \
+             --inventory-file=/tmp/inventory \
+             --module-name=git \
+             --url=https://github.com/${PROJECT} \
+             --verbose \
+             --only-if-changed \
+             --user vagrant \
+             playbook.yml
 
